1a5197c80719d94c8a9bf7cb6a83ff6418088bf8
Jeffrey Zhang <jczhang@umich.edu>
2018-10-22 23:11:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/ThePuppyApp.java
@@ -0,0 +1,13 @@
+package com.example.jeffrey_zhang.thepuppyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThePuppyApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_puppy_app);
+    }
+}
