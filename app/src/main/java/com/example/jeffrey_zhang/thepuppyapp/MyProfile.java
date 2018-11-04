75c11a4fd92b3a0c8600653a2c6c9f52ce1bd252
husaint <htaher@umich.edu>
2018-11-04 16:15:26 -0500

    Added My Profile

---
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyProfile.java
@@ -0,0 +1,14 @@
+package com.example.jeffrey_zhang.thepuppyapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MyProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+
+}
