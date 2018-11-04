7288b1b8528ee94ad1eaeea6c74ab4297c8f635e
Jeffrey Zhang <jczhang@umich.edu>
2018-11-04 15:39:05 -0500

    11/4 Login Page Finished Commit

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/ThePuppyApp.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/ThePuppyApp.java
@@ -1,13 +1,150 @@
 package com.example.jeffrey_zhang.thepuppyapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ThePuppyApp extends Activity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+// Implement ClickListener
+public class ThePuppyApp extends Activity implements View.OnClickListener {
+
+    // Initialize objects
+    private Button buttonLogin, buttonRegister;
+    private EditText editTextEmail, editTextPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_puppy_app);
+
+        // Attach objects to Layout widgets
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        // Make button clickable
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "on.AuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String password, email;
+
+        password = editTextPassword.getText().toString();
+        email = editTextEmail.getText().toString();
+
+        // Button to log in
+        if (view == buttonLogin) {
+            signIn(email, password);
+        }
+
+        // Button to register account
+        else if (view == buttonRegister) {
+            createAccount(email, password);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void createAccount (String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(ThePuppyApp.this, "Authentication " +
+                                    "successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(ThePuppyApp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signIn (String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            if (user != null) {
+                                // Name, email address, and profile photo URL
+                                String name = user.getDisplayName();
+                                String email = user.getEmail();
+                                Uri photoUrl = user.getPhotoUrl();
+
+                                // The user's ID, unique to the Firebase project. Do NOT use this value to
+                                // authenticate with your backup server, if you have one. User
+                                // FirebaseUser.getToken() instead.
+                                String uid = user.getUid();
+                            }
+                            startActivity(new Intent(ThePuppyApp.this, HomePage.class));
+                            Toast.makeText(ThePuppyApp.this, "Logged in",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(ThePuppyApp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
