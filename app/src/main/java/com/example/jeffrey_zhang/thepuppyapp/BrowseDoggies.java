9dc9ff7d503baeb0e5acef4c2becaf3364438687
husaint <htaher@umich.edu>
2018-11-04 18:21:49 -0500

    Added My Profile

---
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/BrowseDoggies.java
@@ -0,0 +1,14 @@
+package com.example.jeffrey_zhang.thepuppyapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BrowseDoggies extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_doggies);
+    }
+
+}
