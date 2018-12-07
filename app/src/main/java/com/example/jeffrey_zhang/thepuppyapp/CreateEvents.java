7801ced032f77344bfca43777bb3af939ddd58f7
Jeffrey Zhang <jczhang@umich.edu>
2018-12-07 13:34:11 -0500

    12/6 CreateEvents Activity commit

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/CreateEvents.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/CreateEvents.java
@@ -2,12 +2,18 @@ package com.example.jeffrey_zhang.thepuppyapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class CreateEvents extends Activity {
+public class CreateEvents extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_events);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
