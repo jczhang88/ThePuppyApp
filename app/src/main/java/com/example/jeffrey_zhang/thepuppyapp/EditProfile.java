62331da0ac0f512a31dded46e167e83a9cd6f634
husaint <htaher@umich.edu>
2018-12-08 14:29:51 -0500

    Building out myProfile and puppyProfile

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/EditProfile.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/EditProfile.java
@@ -1,13 +1,214 @@
 package com.example.jeffrey_zhang.thepuppyapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class EditProfile extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class EditProfile extends Activity implements View.OnClickListener {
+
+    private final int PICK_IMAGE_REQUEST = 1;
+    private Button buttonSelectImage;
+    private Button buttonUploadImage;
+    private ImageView imageViewProfilePicture;
+    private StorageReference mStorageRef;
+    private Uri selectedImageUri;
+    private FirebaseAuth mAuth;
+    private EditText editTextDisplayName;
+    private Button buttonEditName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        buttonSelectImage = findViewById(R.id.buttonSelectImage);
+        buttonUploadImage = findViewById(R.id.buttonUploadImage);
+        imageViewProfilePicture = findViewById(R.id.imageViewProfilePicture);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        editTextDisplayName= findViewById(R.id.editTextDisplayName);
+        buttonEditName = findViewById(R.id.buttonEditName);
+
+        buttonSelectImage.setOnClickListener(this);
+        buttonUploadImage.setOnClickListener(this);
+        buttonEditName.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater optionsMenuInflater = getMenuInflater();
+        optionsMenuInflater.inflate(R.menu.dropdown_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.myProfileMenuItem:
+                startActivity(new Intent(this, MyProfile.class));
+                return true;
+            case R.id.homepageMenuItem:
+                startActivity(new Intent(this, HomePage.class));
+                return true;
+            case R.id.puppyPlaydatesMenuItem:
+                startActivity(new Intent(this, PuppyPlaydates.class));
+                return true;
+            case R.id.barkBoardMenuItem:
+                startActivity(new Intent(this, BarkBoard.class));
+                return true;
+            case R.id.fetchMenuItem:
+                startActivity(new Intent(this, BrowseDoggies.class));
+                return true;
+            case R.id.mapMenuItem:
+                startActivity(new Intent(this, Map.class));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSelectImage){
+            selectImages();
+        }
+
+        else if(view == buttonUploadImage){
+            uploadFile();
+        }
+
+        else if(view == buttonEditName){
+            editName();
+        }
+    }
+
+    private void selectImages(){
+        Intent intent = new Intent();
+        // Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+            selectedImageUri = uri;
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                imageViewProfilePicture = (ImageView) findViewById(R.id.imageViewProfilePicture);
+                imageViewProfilePicture.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadFile(){
+        FirebaseUser user = mAuth.getCurrentUser();
+        StorageReference propicRef = mStorageRef.child("images/userProfilePics/" + user.getUid()
+                + "/");
+
+        propicRef.putFile(selectedImageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Toast.makeText(EditProfile.this, "Upload Successful",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+    private void editName(){
+        final FirebaseUser user = mAuth.getCurrentUser();
+        final String newName = editTextDisplayName.getText().toString().trim();
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("users");
+
+        Query name_change = myRef.orderByChild("emailAddress").equalTo(user.getEmail());
+
+        name_change.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    User editUser = dataSnapshot.getValue(User.class);
+
+                    editUser.setDisplayName(newName);
+
+                    myRef.child(user.getUid()).setValue(editUser);
+
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyProfile.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyProfile.java
@@ -1,12 +1,24 @@
 package com.example.jeffrey_zhang.thepuppyapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -15,13 +27,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class MyProfile extends Activity {
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+public class MyProfile extends Activity implements View.OnClickListener {
 
     private TextView textViewMyProfileName;
     private TextView textViewMyProfileBio;
-    private Button editProfile;
+    private Button buttonEditProfile;
     private FirebaseAuth mAuth;
+    private ImageView imageViewProfilePicture;
+    private StorageReference mStorageRef2;
     
 
     @Override
@@ -31,10 +52,17 @@ public class MyProfile extends Activity {
 
         textViewMyProfileName = findViewById(R.id.textViewMyProfileName);
         textViewMyProfileBio = findViewById(R.id.textViewMyProfileBio);
+        buttonEditProfile = findViewById(R.id.buttonEditProfile);
+        imageViewProfilePicture = findViewById(R.id.imageViewProfilePicture);
+        mStorageRef2 = FirebaseStorage.getInstance().getReference();
+        try{
+            downloadAndSetImage();
+        } catch(IOException exc){
+
+        }
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
-        editProfile = findViewById(R.id.buttonEditProfile);
 
 
 
@@ -72,10 +100,78 @@ public class MyProfile extends Activity {
             }
         });
 
+        buttonEditProfile.setOnClickListener(this);
+
 
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater optionsMenuInflater = getMenuInflater();
+        optionsMenuInflater.inflate(R.menu.dropdown_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.myProfileMenuItem:
+                startActivity(new Intent(this, MyProfile.class));
+                return true;
+            case R.id.homepageMenuItem:
+                startActivity(new Intent(this, HomePage.class));
+                return true;
+            case R.id.puppyPlaydatesMenuItem:
+                startActivity(new Intent(this, PuppyPlaydates.class));
+                return true;
+            case R.id.barkBoardMenuItem:
+                startActivity(new Intent(this, BarkBoard.class));
+                return true;
+            case R.id.fetchMenuItem:
+                startActivity(new Intent(this, BrowseDoggies.class));
+                return true;
+            case R.id.mapMenuItem:
+                startActivity(new Intent(this, Map.class));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void downloadAndSetImage() throws IOException {
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        StorageReference profilePicRef = mStorageRef2.child("images/userProfilePics/"
+                + user.getUid() + "/");
+        final File localFile = File.createTempFile("images", "jpg");
+        profilePicRef.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // Successfully downloaded data to local file
+                        // ..
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(localFile));
+                            imageViewProfilePicture.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle failed download
+                // ...
+            }
+        });
+    }
 
 
+    @Override
+    public void onClick(View view) {
+        if(view == buttonEditProfile){
+            startActivity(new Intent(MyProfile.this, EditProfile.class));
+        }
+    }
 }
