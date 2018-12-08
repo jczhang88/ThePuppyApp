3d7255eaae6b9de397540819ab22169390b9c038
Jeffrey Zhang <jczhang@umich.edu>
2018-12-08 13:39:39 -0500

    12/8 Playdates

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/CreateEvents.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/CreateEvents.java
@@ -3,17 +3,38 @@ package com.example.jeffrey_zhang.thepuppyapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateEvents extends Activity implements View.OnClickListener {
+
+    private Button buttonCreateYourEvent;
+    private EditText editTextCreateEventName, editTextCreateEventLocation, editTextCreateEventDate,
+                     editTextCreateEventDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_events);
+
+        buttonCreateYourEvent = findViewById(R.id.buttonCreateYourEvent);
+        editTextCreateEventName = findViewById(R.id.editTextCreateEventName);
+        editTextCreateEventLocation = findViewById(R.id.editTextCreateEventLocation);
+        editTextCreateEventDate = findViewById(R.id.editTextCreateEventDate);
+        editTextCreateEventDescription = findViewById(R.id.editTextCreateEventDescription);
+
+        buttonCreateYourEvent.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        if (view == buttonCreateYourEvent) {
+            String eventName = editTextCreateEventName.getText().toString();
+            String eventLocation = editTextCreateEventLocation.getText().toString();
+            String eventDate = editTextCreateEventDate.getText().toString();
+            String eventDescription = editTextCreateEventDescription.getText().toString();
 
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/Events.java
@@ -0,0 +1,12 @@
+package com.example.jeffrey_zhang.thepuppyapp;
+
+public class Events {
+    String eventName;
+    String eventLocation;
+    String eventDate;
+    String eventDescription;
+
+    public Events () {
+
+    }
+}
