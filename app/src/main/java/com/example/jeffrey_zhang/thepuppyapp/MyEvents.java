81b5e68b128420c78c44f92b6ebc4ec19fa8bd83
Jeffrey Zhang <jczhang@umich.edu>
2018-12-09 20:50:42 -0500

    12/9 Playdates List Layout and Recycler View Adapter Without Firebase

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyEvents.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyEvents.java
@@ -2,12 +2,32 @@ package com.example.jeffrey_zhang.thepuppyapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MyEvents extends Activity {
+
+    private ArrayList<Event> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
+        initEvents();
+    }
+
+    private void initEvents() {
+        events = new ArrayList<>();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerViewMyEvents = findViewById(R.id.recyclerViewMyEvents);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(events, this);
+        recyclerViewMyEvents.setAdapter(recyclerViewAdapter);
+        recyclerViewMyEvents.setLayoutManager(new LinearLayoutManager(this));
     }
 }
