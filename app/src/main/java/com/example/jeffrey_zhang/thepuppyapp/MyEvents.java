1f87c4cf24dc77602a227f984a736f7277029967
Jeffrey Zhang <jczhang@umich.edu>
2018-12-09 21:17:50 -0500

    12/9 Playdates List Layout and Recycler View Adapter With Firebase

--- app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyEvents.java
+++ app/src/main/java/com/example/jeffrey_zhang/thepuppyapp/MyEvents.java
@@ -3,30 +3,67 @@ package com.example.jeffrey_zhang.thepuppyapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MyEvents extends Activity {
 
     private ArrayList<Event> events;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
-        initEvents();
-    }
 
-    private void initEvents() {
         events = new ArrayList<>();
         initRecyclerView();
+        getEvents();
+    }
+
+    private void getEvents() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference eventsRef = database.getReference("events").child(uid);
+
+        // Read from the database
+        eventsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Event event = child.getValue(Event.class);
+                    events.add(event);
+                }
+                recyclerViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void initRecyclerView() {
         RecyclerView recyclerViewMyEvents = findViewById(R.id.recyclerViewMyEvents);
-        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(events, this);
+        recyclerViewAdapter = new RecyclerViewAdapter(events, this);
         recyclerViewMyEvents.setAdapter(recyclerViewAdapter);
         recyclerViewMyEvents.setLayoutManager(new LinearLayoutManager(this));
     }
